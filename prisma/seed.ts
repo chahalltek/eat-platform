7d0818c98d58b111df03038f423d6643a5fca29b
chahalltek <chahall@gmail.com>
2025-12-07 14:07:38 -0500

    Add seeding script for charlie user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,14 +2,18 @@ import { prisma } from '../src/lib/prisma';
 
 async function main() {
   await prisma.user.upsert({
-    where: { id: "charlie" },
-    update: {},
+    where: { id: 'charlie' },
+    update: {
+      email: 'charlie@strategicsystems.io',
+      name: 'Charlie Hall',
+      role: 'RECRUITER',
+    },
     create: {
-      id: "charlie",
-      email: "chall@strsi.com",
-      name: "Charlie Hall",
-      role: "RECRUITER"
-    }
+      id: 'charlie',
+      email: 'charlie@strategicsystems.io',
+      name: 'Charlie Hall',
+      role: 'RECRUITER',
+    },
   });
 }
 
