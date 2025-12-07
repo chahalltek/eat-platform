5190944ae30e3a8add581c555e1c2221d42f76ed
chahalltek <chahall@gmail.com>
2025-12-07 10:12:37 -0500

    correction for prisma 7 sytax

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+// prisma.config.ts
+// @ts-nocheck
+
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+});
